fced00dee07f7a158ed34bbdcd672ddc48db16ae
Nathan Plamondon <nplamondon@gmail.com>
2025-12-03 09:01:44 -0700

    day03: Solve part 2

--- day03/__init__.py
+++ day03/__init__.py
@@ -4,7 +4,7 @@ Lobby
 '''
 
 # TODO: Add sample solutions
-SAMPLE_SOLUTIONS = [357]
+SAMPLE_SOLUTIONS = [357, 3121910778619]
 
 
 def parse_data(dataset: list) -> list:
@@ -42,6 +42,23 @@ def solve_1(dataset: list) -> int:
 def solve_2(dataset: list) -> int:
     '''Solve part 2'''
 
+    joltage = 0
+
     for item in dataset:
-        # TODO: Build solution
-        pass
+        # Work backward from the 12th-to-last digit. Find the earliest largest digit.
+        digits = ''
+        last_pos = -1
+
+        for digits_remaining in range(12, 0, -1):
+            largest = '0'
+            largest_pos = -1
+            for pos in range(len(item) - digits_remaining, last_pos, -1):
+                if item[pos] >= largest:
+                    largest = item[pos]
+                    largest_pos = pos
+            digits += largest
+            last_pos = largest_pos
+
+        joltage += int(digits)
+
+    return joltage
