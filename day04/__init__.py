ec1eaf32f3b59ebe2f9fa0e3c72ae83f579a9fe4
Nathan Plamondon <nplamondon@gmail.com>
2025-12-04 14:02:35 -0700

    day04: Solve part 2

--- day04/__init__.py
+++ day04/__init__.py
@@ -6,7 +6,7 @@ Printing Department
 from shared.helpers import get_eight_neighbors
 
 
-SAMPLE_SOLUTIONS = [13]
+SAMPLE_SOLUTIONS = [13, 43]
 
 
 def parse_data(dataset: list) -> list:
@@ -41,6 +41,36 @@ def solve_1(dataset: list) -> int:
 def solve_2(dataset: list) -> int:
     '''Solve part 2'''
 
-    for item in dataset:
-        # TODO: Build solution
-        pass
+    rows = len(dataset)
+    cols = len(dataset[0])
+    new_dataset = []
+    prev_accessible_rolls = -1
+    accessible_rolls = 0
+
+    while accessible_rolls != prev_accessible_rolls:
+        prev_accessible_rolls = accessible_rolls
+
+        for row in range(rows):
+            new_dataset.append('')
+            for col in range(cols):
+                neighbor_rolls = 0
+
+                if dataset[row][col] == '@':
+                    neighbors = get_eight_neighbors(dataset, (row, col))
+                    for neighbor in neighbors:
+                        if dataset[neighbor[0]][neighbor[1]] == '@':
+                            neighbor_rolls += 1
+
+                    if neighbor_rolls < 4:
+                        accessible_rolls += 1
+                        new_dataset[row] += '.'
+                    else:
+                        new_dataset[row] += '@'
+                else:
+                    new_dataset[row] += '.'
+
+        dataset = new_dataset
+        new_dataset = []
+
+
+    return accessible_rolls
